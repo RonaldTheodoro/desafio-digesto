c8afbeb58b9805b15ba26a08705084f00c2f34c3
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2018-04-14 21:18:31 -0300

    add method get_machine

--- downloaders/vultr.py
+++ downloaders/vultr.py
@@ -2,33 +2,32 @@ from .downloader import Downloader, Machine
 
 
 class VultrPriceTable(Downloader):
+    """Get price info from Vultr"""
     site = 'VULTR'
 
     def get_info(self):
-        machines = []
         elements = self.html.find_all('a', {'class': 'package'})
-        for element in elements:
-            header = element.find('div', {'class': 'package-header'})
-            body = element.find('div', {'class': 'package-body'})
-            items = body.ul.find_all('li')
+        return map(self.get_machine, elements)
 
-            storage = header.h3.text
-            price_hour = header.span.attrs['data-hourly']
-            price_month = header.span.attrs['data-monthly']
-            cpus = items[0].text
-            memory = items[1].text
-            bandwidth = items[2].text
+    def get_machine(self, element):
+        """Get one machine"""
+        header = element.find('div', {'class': 'package-header'})
+        body = element.find('div', {'class': 'package-body'})
+        items = body.ul.find_all('li')
 
-            machine = Machine(
-                site=self.site,
-                name=storage,
-                cpus=cpus,
-                memory=memory,
-                storage=storage,
-                bandwidth=bandwidth,
-                price_month=price_month,
-                price_hour=price_hour
-            )
+        storage = header.h3.text
+        price_hour = header.span.attrs['data-hourly']
+        price_month = header.span.attrs['data-monthly']
+        cpus = items[0].text
+        memory = items[1].text
+        bandwidth = items[2].text
 
-            machines.append(machine)
-        return machines
+        return self.get_machine_obj(
+            storage,
+            cpus,
+            memory,
+            storage,
+            bandwidth,
+            price_month,
+            price_hour
+        )
