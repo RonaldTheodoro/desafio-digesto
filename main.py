bf480d5b7a8e2f9ed6f9a8cfd1b2ceae98d5f662
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2018-04-19 01:10:11 -0300

    use arg parser

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-from downloaders import VultrPriceTable, DigitalOceanPriceTable
+from argparse import ArgumentParser
+
+from dispatcher import Dispatcher
 
 
 if __name__ == '__main__':
-    vultr = VultrPriceTable()
-    machines = vultr.get_info()
-    for machine in machines:
-        print(machine)
+    parser = ArgumentParser(
+        description='A simple tool for get the price from cloud services'
+    )
+    parser.add_argument(
+        '-s',
+        '--services',
+        default='all',
+        choices=['vultr', 'digitalocean'],
+        help='Choice an individual service to get the data'
+    )
+    args = parser.parse_args()
+    dispatcher = Dispatcher(args.services)
