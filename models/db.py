9155348c3d27bed593a59c51db68e78412d6dc19
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2018-04-19 04:08:56 -0300

    add docstring in Database class and methods

--- models/db.py
+++ models/db.py
@@ -7,19 +7,23 @@ from .models import Base, Machine
 
 
 class Database:
+    """Handle the database access"""
     engine = create_engine(Config.DATABASE, echo=False)
     Base.metadata.bind = engine
     Session = sessionmaker(bind=engine)
     session = Session()
 
     def save_machine_bulk(self, machine):
+        """Save all machines"""
         self.session.bulk_save_objects(machine)
         self.session.commit()
 
     def get_all_machines(self):
+        """Get all registers from db"""
         return self.session.query(Machine).all()
 
     def delete_all(self):
+        """Delete all registers"""
         rows = 0
         try:
             rows = self.session.query(Machine).delete()
