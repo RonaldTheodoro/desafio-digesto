52c159a1a8f8ebff8ec7b1ed24f57ce951309e54
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2018-04-19 02:02:57 -0300

    add clean_field method

--- models/models.py
+++ models/models.py
@@ -1,24 +1,28 @@
+import re
+
+
 class Machine:
 
     def __init__(self, site='', name='', cpus='', memory='', storage='', bandwidth='', price_month='', price_hour=''):
-        self.site = site
-        self.name = name
-        self.cpus = cpus
-        self.memory = memory
-        self.storage = storage
-        self.bandwidth = bandwidth
-        self.price_month = price_month
-        self.price_hour = price_hour
-
-    def __str__(self):
-        return f'{self.site} - {self.name}'
+        self.site = self.clean_field(site)
+        self.name = self.clean_field(name)
+        self.cpus = self.clean_field(cpus)
+        self.memory = self.clean_field(memory)
+        self.storage = self.clean_field(storage)
+        self.bandwidth = self.clean_field(bandwidth)
+        self.price_month = self.clean_field(price_month)
+        self.price_hour = self.clean_field(price_hour)
 
     def show_machine_info(self):
-        print('site', self.site)
-        print('name', self.name)
-        print('cpus', self.cpus)
-        print('memory', self.memory)
-        print('storage', self.storage)
-        print('bandwidth', self.bandwidth)
-        print('price_month', self.price_month)
-        print('price_hour', self.price_hour)
+        print(f'site: {self.site}')
+        print(f'name: {self.name}')
+        print(f'cpus: {self.cpus}')
+        print(f'memory: {self.memory}')
+        print(f'storage: {self.storage}')
+        print(f'bandwidth: {self.bandwidth}')
+        print(f'price_month: {self.price_month}')
+        print(f'price_hour: {self.price_hour}')
+
+    def clean_field(self, field):
+        """Remove whitespaces"""
+        return re.sub(r'\s+', ' ', field).strip()
